92ce416c6f1ae3435cc9ae5c7077c42ddaf093dd
Jesus <chus.working@gmail.com>
2018-11-14 16:34:30 +0100

    Primer esqueleto creado

---
+++ app/src/main/java/edu/uoc/curs/bookshare/UploadBook.java
@@ -0,0 +1,13 @@
+package edu.uoc.curs.bookshare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UploadBook extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_book);
+    }
+}
