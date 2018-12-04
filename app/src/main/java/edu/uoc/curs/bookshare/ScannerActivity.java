667d6ef64aaba2f6faafa1bcf51e551c54a0d6bd
Jesus <chus.working@gmail.com>
2018-12-04 20:06:31 +0100

    API Google Books Bug fixes

--- app/src/main/java/edu/uoc/curs/bookshare/ScannerActivity.java
+++ app/src/main/java/edu/uoc/curs/bookshare/ScannerActivity.java
@@ -5,7 +5,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Toast;
 
 import java.util.ArrayList;
--- app/src/main/java/edu/uoc/curs/bookshare/UploadBook.java
+++ app/src/main/java/edu/uoc/curs/bookshare/UploadBook.java
@@ -30,7 +30,6 @@ import java.net.URL;
 public class UploadBook extends AppCompatActivity {
 
     private static final String bookUrl = "https://www.googleapis.com/books/v1/volumes?q=isbn:";
-    private static final String keyAPI = "&AIzaSyAp9NMAAUs7uiGTuOu4eWUqbESR4eETuaw";
 
     private Button scan;
     private TextView authorText, titleText, descriptionText, dateText, isbn10, isbn13;
@@ -57,7 +56,7 @@ public class UploadBook extends AppCompatActivity {
 
         //Se prepara la url para que el AsyncTask haga la búsqueda en la API Books
         //de Google, siempre de la misma manera.
-        String bookSearch = bookUrl + isbn + keyAPI;
+        String bookSearch = bookUrl + isbn;
         //Mostraremos un diálogo para que el usuario sepa que está ocurriendo algo mientras
         //se ejecuta la AsyncTask
         dialog = ProgressDialog.show(UploadBook.this, "Please wait",
@@ -166,36 +165,68 @@ public class UploadBook extends AppCompatActivity {
                     JSONArray bookArray = responseJson.getJSONArray("items");
                     JSONObject bookObject = bookArray.getJSONObject(0);
                     JSONObject volumeObject = bookObject.getJSONObject("volumeInfo");
-                    JSONArray authorArray = volumeObject.getJSONArray("authors");
 
-                    //El libro puede tener más de un autor
-                    StringBuilder authorBuild = new StringBuilder();
-                    for (int i = 0; i < authorArray.length(); i++) {
-                        if (i > 0) authorBuild.append(", ");
-                        authorBuild.append(authorArray.getString(i));
+                    try {
+                        titleText.setText(volumeObject.getString("title"));
+                    } catch (JSONException jse) {
+                        titleText.setText("TITLE NOT FOUND");
+                        jse.printStackTrace();
                     }
 
-                    titleText.setText(volumeObject.getString("title"));
-                    authorText.setText(authorBuild.toString());
-                    dateText.setText(volumeObject.getString("publishedDate"));
-                    descriptionText.setText(volumeObject.getString("description"));
+                    //El libro puede tener más de un autor
+                    try {
+                        JSONArray authorArray = volumeObject.getJSONArray("authors");
+                        StringBuilder authorBuild = new StringBuilder();
+                        for (int i = 0; i < authorArray.length(); i++) {
+                            if (i > 0) authorBuild.append(", ");
+                            authorBuild.append(authorArray.getString(i));
+                        }
+                        authorText.setText(authorBuild.toString());
+                    } catch (JSONException jse) {
+                        authorText.setText("AUTHOR NOT FOUND");
+                        jse.printStackTrace();
+                    }
 
-                    JSONObject imageInfo = volumeObject.getJSONObject("imageLinks");
-                    String imageUrl = imageInfo.getString("thumbnail");
-                    Picasso.get().load(imageUrl).into(thumbView);
+                    try {
+                        dateText.setText(volumeObject.getString("publishedDate"));
+                    } catch (JSONException jse) {
+                        dateText.setText("PUBLICATION DATE NOT FOUND");
+                        jse.printStackTrace();
+                    }
+
+                    try {
+                        descriptionText.setText(volumeObject.getString("description"));
+                    } catch (JSONException jse) {
+                        descriptionText.setText("DESCRIPTION NOT FOUND");
+                        jse.printStackTrace();
+                    }
+
+                    try {
+                        JSONObject imageInfo = volumeObject.getJSONObject("imageLinks");
+                        String imageUrl = imageInfo.getString("thumbnail");
+                        Picasso.get().load(imageUrl).into(thumbView);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
 
                     //Los identificadores ISBN10 e ISBN13 no siempre están en el mismo orden
                     //en la respuesta que decibimos, por lo que el primer objeto dentro del
                     //array puede ser el 10 o el 13 indistintamente.
-                    JSONArray isbnArray = volumeObject.getJSONArray("industryIdentifiers");
-                    JSONObject isbnObject1 = isbnArray.getJSONObject(0);
-                    JSONObject isbnObject2 = isbnArray.getJSONObject(1);
-                    if ("ISBN_13".equals(isbnObject1.getString("type"))){
-                        isbn10.setText("ISBN 10: " + isbnObject2.getString("identifier"));
-                        isbn13.setText("ISBN 13: " + isbnObject1.getString("identifier"));
-                    } else {
-                        isbn10.setText("ISBN 10: " + isbnObject1.getString("identifier"));
-                        isbn13.setText("ISBN 13: " + isbnObject2.getString("identifier"));
+                    try {
+                        JSONArray isbnArray = volumeObject.getJSONArray("industryIdentifiers");
+                        JSONObject isbnObject1 = isbnArray.getJSONObject(0);
+                        JSONObject isbnObject2 = isbnArray.getJSONObject(1);
+                        if ("ISBN_13".equals(isbnObject1.getString("type"))) {
+                            isbn10.setText("ISBN 10: " + isbnObject2.getString("identifier"));
+                            isbn13.setText("ISBN 13: " + isbnObject1.getString("identifier"));
+                        } else {
+                            isbn10.setText("ISBN 10: " + isbnObject1.getString("identifier"));
+                            isbn13.setText("ISBN 13: " + isbnObject2.getString("identifier"));
+                        }
+                    } catch (JSONException jse) {
+                        isbn10.setText("ISBN10 NOT FOUND");
+                        isbn13.setText("ISBN13 NOT FOUND");
+                        jse.printStackTrace();
                     }
 
                     //Se cierra el diálogo cuando toda la información está lista para presentarse
@@ -203,6 +234,8 @@ public class UploadBook extends AppCompatActivity {
                     dialog.dismiss();
                 } catch (JSONException e) {
                     // TODO: hacer algo con error
+                    titleText.setText("BOOK NOT FOUND");
+                    dialog.dismiss();
                     Log.e("BookApi.class", e.toString());
                 }
             }
